62d77244a14b0d7588f2463a583fb6801e93bb0a
Hans <741581879@qq.com>
2020-08-27 19:40:16 +0800

    [[refactor practice][Hans] refactor: Extract format method

--- src/statement.js
+++ src/statement.js
@@ -5,15 +5,18 @@ function calculateAmount(thisAmount, perf, boundary, unitIncrease, startingNumbe
   return thisAmount;
 }
 
+function format(thisAmount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(thisAmount / 100);
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -35,10 +38,10 @@ function statement(invoice, plays) {
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
     //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${format(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 }
